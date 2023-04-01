a7d355d0680fa87d1d16574bef18f123fe227ba9
BhanuPratap-Singh-Naruka <pratapbhanu0187@gmail.com>
2023-04-01 09:58:01 +0530

    feat: configured eslint, prettier and husky

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
-import '@/styles/globals.css'
+/* eslint-disable react/prop-types */
+import '../styles/globals.css';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
