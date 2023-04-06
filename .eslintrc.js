cd8480deff141d0d427e4930f9d01d1dbe59817b
BhanuPratap-Singh-Naruka <pratapbhanu0187@gmail.com>
2023-04-06 21:23:57 +0530

    feat: set up unit test

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,15 +9,21 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:prettier/recommended',
   ],
-  overrides: [],
+  overrides: [
+    {
+      files: ['**/__test__/**/*.[jt]s?(x)', '**/?(*.)+(spec |test).[jt]s?(x)'],
+      extends: ['plugin:testing-library/react'],
+    },
+  ],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: ['react', 'prettier'],
+  plugins: ['react', 'prettier', 'testing-library'],
   rules: {
     'react/react-in-jsx-scope': 0,
     'react/jsx-props-no-spreading': 0,
     'react/jsx-filename-extension': 0,
+    'no-undef': 'off',
   },
 };
